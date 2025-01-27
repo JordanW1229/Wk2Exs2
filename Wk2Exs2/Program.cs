e60b81c6c9a771969d7465cd441bc18e1179cf18
Jwellls1229 <Jwellls1229@BB21317>
2025-01-27 13:02:18 -0600

    Add project files.

---
+++ Wk2Exs2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wk2Exs2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //get the length of the rectangle
+            Console.WriteLine("what is the length of the rectangle?");
+            double length = Convert.ToDouble(Console.ReadLine());
+
+            //get width
+            Console.WriteLine("What is the width of the rectangle?");
+            double width = Convert.ToDouble(Console.ReadLine());
+
+
+            //caculate area of rectangle
+            double area = length * width;
+            Console.WriteLine("The Area of the rectangle is" + area);
+
+            //Calculate perimeter 
+            double perimeter = length + length + width + width;
+            Console.WriteLine("The perimeter of the recatangle is" + perimeter);
+
+            //pause for user to see
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
